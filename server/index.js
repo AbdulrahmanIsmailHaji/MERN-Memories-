c6f43fd5d7a740eaa9959ae044847a343a3a4f96
Abdulrahman Ismail <abdulrahmanismail987@gmail.com>
2024-01-16 15:10:09 +0300

    adding cors origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://mern-memories-delta.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 app.use("/posts", postRoutes);
 app.use("/user", userRouter);
